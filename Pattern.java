34f758d36faec6342e1807b41ed2405361e250c9
Nandini Sharma <nandiniisharma@outlook.com>
2020-09-20 23:15:03 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -1,26 +1,21 @@
-import java.util.Scanner;
-public class Pattern {
-	
-	public static void main(String[] args) {
-		
-		Scanner obj = new Scanner(System.in);
-		System.out.println("enter 1 number");
-		
-		String s=obj.nextLine();
-		int k=Integer.parseInt(s);
-		
-		for(int i = 1; i<=k; i++) {
-			
-			for(int j=k ;j>=i;j--) {
-				System.out.print("*");
-				
-			}
-			System.out.println();
-		}
-		
-		
-		
-	}
-	
-
+public class Pattern   
+{   
+public static void main(String args[])   
+{   
+//i for rows and j for columns      
+//row denotes the number of rows you want to print  
+int i, j, row=6;   
+//outer loop for rows  
+for(i=0; i<row; i++)   
+{   
+//inner loop for columns  
+for(j=0; j<=i; j++)   
+{   
+//prints stars   
+System.out.print("* ");   
+}   
+//throws the cursor in a new line after printing each line  
+System.out.println();   
+}   
+}   
 }
